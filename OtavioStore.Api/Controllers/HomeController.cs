5a3ffd7bf82db3bd8b92c56f5890455cfe64827e
caotavio <otavio.araujodev@gmail.com>
2019-11-11 18:03:54 +0000

    deployment config

--- OtavioStore.Api/Controllers/HomeController.cs
+++ OtavioStore.Api/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using OtavioStore.Domain.StoreContext.Entities;
@@ -11,6 +12,14 @@ namespace OtavioStore.Api.Controllers
         public List<Customer> Get()
         {
             return null;
+        }
+
+        [HttpGet]
+        [Route("error")]
+        public string Error()
+        {
+            throw new Exception("Algum erro ocorreu");
+            return "erro";
         }
     }
 }
--- OtavioStore.Api/Startup.cs
+++ OtavioStore.Api/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using OtavioStore.Domain.StoreContext.Handlers;
 using OtavioStore.Domain.StoreContext.Repositories;
@@ -8,13 +7,27 @@ using OtavioStore.Domain.StoreContext.Services;
 using OtavioStore.Infra.DataContexts;
 using OtavioStore.Infra.StoreContext.Repositories;
 using OtavioStore.Infra.StoreContext.Services;
+using Swashbuckle.AspNetCore.Swagger;
+using Elmah.Io.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using OtavioStore.Shared;
 
 namespace OtavioStore.Api
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            services.AddApplicationInsightsTelemetry(Configuration);
             services.AddMvc();
             
             services.AddResponseCompression();
@@ -23,6 +36,13 @@ namespace OtavioStore.Api
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<CustomerHandler, CustomerHandler>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "Otavio Store", Version = "v1" });
+            });
+
+            Settings.ConnectionString = $"{Configuration["connectionString"]}";
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +53,15 @@ namespace OtavioStore.Api
             app.UseMvc();
             
             app.UseResponseCompression();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Otavio Store - V1");
+            });
+
+            app.UseElmahIo();
         }
     }
 }
